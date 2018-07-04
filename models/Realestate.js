8d1b6b646fc2b6070a68938226a27441dcac3a49
rpang27 <rpang27@bloomberg.net>
2018-07-03 21:13:48 -0400

    wip

--- models/Realestate.js
+++ models/Realestate.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const RealestateSchema = new mongoose.Schema({
   name: String,
   address: String,
+  phoneNumber: { area: Number, exchange: Number, subscriber: Number },
   price: Number,
+  groupPrice: Number,
   description: String,
   images: [String]
 });
