a095d90b9ca696c94480ab94042e7874189e8ac1
rpang27 <rpang27@bloomberg.net>
2018-07-13 18:02:24 -0400

    wip

--- models/Realestate.js
+++ models/Realestate.js
@@ -3,11 +3,17 @@ const mongoose = require('mongoose');
 const RealestateSchema = new mongoose.Schema({
   name: String,
   address: String,
-  phoneNumber: { area: Number, exchange: Number, subscriber: Number },
   price: Number,
+  redpocket: { amount: Number, startDate: String, endDate: String },
   groupPrice: Number,
+  phone: Number,
+  consultant: { title: String, phone: Number, description: String },
   description: String,
-  images: [String]
+  images: [String],
+  visitServices: [{ name: String, description: String }],
+  comments: [{ account: String, text: String, date: String }],
+  relatedRealestateIds: [String]
 });
+
 
 module.exports = mongoose.model('Realestate', RealestateSchema);
